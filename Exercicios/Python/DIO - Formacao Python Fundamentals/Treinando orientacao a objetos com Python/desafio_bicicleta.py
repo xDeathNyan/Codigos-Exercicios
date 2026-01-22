5cab37c8da8f130ce3b86b9e9de03df738fb3bf6
Guilherme Couto <guilherme.ncouto85@gmail.com>
2026-01-22 20:33:46 +0000

    testes DIO

---
+++ Exercicios/Python/DIO - Formacao Python Fundamentals/Treinando orientacao a objetos com Python/desafio_bicicleta.py
@@ -0,0 +1,25 @@
+class Bicicleta:
+    def __init__(self, cor, modelo, ano, valor):
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano
+        self.valor = valor
+
+
+    def __str__(self):
+        return f"Cor: {self.cor}, Modelo: {self.modelo}, Ano: {self.ano}, Valor: R$ {self.valor:,.2f}"
+    
+    def buzinar(self):
+        print("Buzina: plim plim!")
+
+    def parar(self):
+        print("Parando a bicicleta...")
+        print("Bicicleta parada.")
+
+    def correr(self):
+        print("A bicicleta está em movimento.")
+    
+
+
+b1 = Bicicleta("Vermelha", "Mountain Bike", 2021, 1500.00)
+print(b1)
