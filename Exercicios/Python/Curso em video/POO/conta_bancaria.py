42bf7051f792499fbe9e4392cd067c7c893d0ef9
Guilherme Couto <guilherme.ncouto85@gmail.com>
2026-01-22 18:57:26 +0000

    Conta bancaria arrumada.

--- Exercicios/Python/Curso em video/POO/conta_bancaria.py
+++ Exercicios/Python/Curso em video/POO/conta_bancaria.py
@@ -10,24 +10,35 @@ class ContaBancaria:
         print(f"Conta {self.id} criada para {self.titular} com saldo inicial de R$ {self.saldo:,.2f}.\n")
 
     def __str__(self):
-        return f"A conta {self.id} de {self.titular} tem R$ {self.saldo:,.2f} de saldo.\n"
+        return f"Conta {self.id} - {self.titular}"
+
+    def realizar_extrato(self):
+        linha = "=" * 40
+        titulo = 'EXTRATO'.center(40)
+        print(f"\n{linha}\n{titulo}\n{linha}\n{'ID':<15} | {str(self.id):>20}\n{'Titular':<15} | {self.titular:>20}\n{'Saldo':<15} | {f'R$ {self.saldo:,.2f}':>20}\n{linha}\n")
 
     def depositar(self, valor):
         self.saldo += valor
-        print(f"Depósito de R$ {valor:,.2f} realizado com sucesso na conta {self.id}.\n")
+        linha = "-" * 40
+        titulo = 'Depósito realizado'.center(40)
+        print(f"{linha}\n{titulo}\n{linha}\n{'Valor Depositado':<20} | {f'R$ {valor:,.2f}':>18}\n{'Novo Saldo':<20} | {f'R$ {self.saldo:,.2f}':>18}\n{linha}\n")
 
     def sacar(self, valor):
         if valor > self.saldo:
-            print(f"Saldo insuficiente para saque de R$ {valor:,.2f} na conta {self.id}.\n")
+            linha = "-" * 40
+            titulo = '❌ Erro no Saque'.center(40)
+            print(f"{linha}\n{titulo}\n{linha}\n{'Saldo Disponível':<20} | {f'R$ {self.saldo:,.2f}':>18}\n{'Valor Solicitado':<20} | {f'R$ {valor:,.2f}':>18}\n{linha}\n")
         else:
             self.saldo -= valor
-            print(f"Saque de R$ {valor:,.2f} realizado com sucesso na conta {self.id}.\n")
+            linha = "-" * 40
+            titulo = 'Saque realizado'.center(40)
+            print(f"{linha}\n{titulo}\n{linha}\n{'Valor Sacado':<20} | {f'R$ {valor:,.2f}':>18}\n{'Novo Saldo':<20} | {f'R$ {self.saldo:,.2f}':>18}\n{linha}\n")
 
 
 
 
 
-conta1 = ContaBancaria(1, "João", 1000)
-conta1.depositar(500)
-conta1.sacar(200)
-print(conta1)
+conta1 = ContaBancaria(1, "Letícia", 1000)
+conta1.depositar(50000)
+conta1.sacar(2000)
+conta1.realizar_extrato()
