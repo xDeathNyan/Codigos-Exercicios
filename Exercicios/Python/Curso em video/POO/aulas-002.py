02413121aca02592681bd9a2825b7b2a09f7e52b
Guilherme Couto <guilherme.ncouto85@gmail.com>
2026-01-22 18:18:22 +0000

    alterando métodos

--- Exercicios/Python/Curso em video/POO/aulas-002.py
+++ Exercicios/Python/Curso em video/POO/aulas-002.py
@@ -11,22 +11,25 @@ class Gafanhoto:
         self.nome = nome
         self.idade = idade
 
+    def __str__(self):
+        return f"{self.nome} é gafanhoto(a) e tem {self.idade} anos de idade."
+
     # Métodos de instância
     def aniversario(self):
         self.idade += 1
 
-    def mensagem(self):
-        return f"{self.nome} é gafanhoto(a) e tem {self.idade} anos de idade."
-
+    def __getstate__(self):
+        return f"Estado: nome={self.nome}, idade={self.idade}"
+    
 # Declaração de objetos
 g1 = Gafanhoto("Guilherme", 23)
-print(g1.mensagem())
+print(g1) # Chama o método __str__() implicitamente
 
 g2 = Gafanhoto("Letícia", 24)
 g2.aniversario()
-print(g2.mensagem())
+print(g2) # Chama o método __str__() implicitamente
 
 g3 = Gafanhoto()
-print(g3.mensagem())  # Valor padrão para nome
 
-print(Gafanhoto.__doc__)  # Documentação da classe
+print(g1.__dict__)  # É um atributo - Mostra os atributos do objeto g1
+print(g1.__getstate__())  # É um método - Mostra o estado do objeto g1
