a7332674afec95c719908622be773172a11311b4
Guilherme Couto <guilherme.ncouto85@gmail.com>
2026-01-23 20:45:45 +0000

    Aula Primeiro Programa POO - DIO

--- Exercicios/Python/DIO - Formacao Python Fundamentals/Treinando orientacao a objetos com Python/desafio_bicicleta.py
+++ Exercicios/Python/DIO - Formacao Python Fundamentals/Treinando orientacao a objetos com Python/desafio_bicicleta.py
@@ -1,13 +1,15 @@
 class Bicicleta:
-    def __init__(self, cor, modelo, ano, valor):
+    def __init__(self, cor, modelo, ano, valor, aro=29):
         self.cor = cor
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
+        self.aro = aro
 
 
     def __str__(self):
-        return f"Cor: {self.cor}, Modelo: {self.modelo}, Ano: {self.ano}, Valor: R$ {self.valor:,.2f}"
+    # Criando uma string formatada diretamente do dicionário de atributos
+        return "\n".join([f"{k.upper():>10}: {v}" for k, v in vars(self).items()])
     
     def buzinar(self):
         print("Buzina: plim plim!")
@@ -19,8 +21,20 @@ class Bicicleta:
     def correr(self):
         print("A bicicleta está em movimento.")
     
+    
 
 
+'''
 b1 = Bicicleta("Vermelha", "Mountain Bike", 2021, 1500.00)
-print(b1)
+print(f"Print direto do objeto bicicleta:\n{b1}\n")
+print("Buzinando a bicicleta:")
+b1.buzinar()
+print("\nColocando a bicicleta em movimento:")
+b1.correr()
+print("\n")
+b1.parar()
+'''
+
+b2 = Bicicleta("Azul", "Speed", 2020, 2500.00, aro=26)
+print(b2)
 
